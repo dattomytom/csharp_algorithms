8cfdf1993619fb4a418c9531f94d4dc14db22326
Dat Duc Nguyen <dat.tomytom@gmail.com>
2023-12-12 20:38:06 +0700

    QuickSort and .gitignore

---
+++ QuicksortAlgorithm/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using QuickSort;
+using System.Linq;
+
+Console.WriteLine("Hello, World!");
+//for(int i=0; i< QuickSort.QuickSortMethods.SampleArrays().ToList().Count; i++)
+//{
+//    QuickSortMethods.ReadArray(QuickSortMethods.SampleArrays().ToList()[i]);
+//}
+var summary = BenchmarkRunner.Run<QuickSortMethods>();
